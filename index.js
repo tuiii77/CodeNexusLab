121aa0268d728b58b7a8408d1475161dc237782d
tuiii77 <melisa1845nam1311@outlook.com>
2024-05-10 21:49:04 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const pull = (arr, ...args) => {
-  let argState = Array.isArray(args[0]) ? args[0] : args;
-  let pulled = arr.filter((v, i) => !argState.includes(v));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-};
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let p = l1,
+    q = l2,
+    curr = dummy;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    const x = p !== null ? p.val : 0;
+    const y = q !== null ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
+  }
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return dummy.next;
+}
