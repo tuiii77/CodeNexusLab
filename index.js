21c20dd18ae021136592fbd07b10251844ae790c
tuiii77 <melisa1845nam1311@outlook.com>
2024-05-12 05:38:18 +0700

    feat project

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-function addTwoNumbers(l1, l2) {
-  const dummy = new ListNode(0);
-  let p = l1,
-    q = l2,
-    curr = dummy;
-  let carry = 0;
-  while (p !== null || q !== null) {
-    const x = p !== null ? p.val : 0;
-    const y = q !== null ? q.val : 0;
-    const sum = x + y + carry;
-    carry = Math.floor(sum / 10);
-    curr.next = new ListNode(sum % 10);
-    curr = curr.next;
-    if (p !== null) p = p.next;
-    if (q !== null) q = q.next;
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
   }
-  if (carry > 0) {
-    curr.next = new ListNode(carry);
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
   }
-  return dummy.next;
-}
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
