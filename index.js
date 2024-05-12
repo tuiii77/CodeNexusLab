d5f8d926756cf1fc45400ff0d80a3e1b188bd919
tuiii77 <melisa1845nam1311@outlook.com>
2024-05-13 03:43:41 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const quickSortRandomPivot = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
+function sortedListToBST(head) {
+  if (!head) return null;
+  if (!head.next) return new TreeNode(head.val);
+  let slow = head;
+  let fast = head;
+  let prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
   }
-  const pivotIndex = Math.floor(Math.random() * arr.length);
-  const pivot = arr[pivotIndex];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== pivotIndex) {
-      if (arr[i] <= pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-  }
-  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
-};
+  const root = new TreeNode(slow.val);
+  prev.next = null;
+  root.left = sortedListToBST(head);
+  root.right = sortedListToBST(slow.next);
+  return root;
+}
